51e737cb8f2394fa301f4a8e85c58ab9018a61fe
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-06-22 22:45:49 +0530

    adding Rating.cs and updating main

---
+++ EmployeeDetails/EmployeeDetails/Rating.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDetails
+{
+    public class Rating
+    {
+        public static float AppraisalFunction(int factor)
+
+        {
+            float rating = factor * factor - 2 * factor + 1;
+            return rating;
+        }
+    }
+}
