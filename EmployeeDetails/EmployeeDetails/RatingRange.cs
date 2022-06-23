e3e95f901c5876535d3363c70c4d7a83ae74a5b8
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-06-23 08:37:33 +0530

    adding RatingRange.cs and updating main

---
+++ EmployeeDetails/EmployeeDetails/RatingRange.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDetails
+{   
+    public class RatingRange
+    {
+        int lowerLimit;
+        int upperLimit;
+        public void GetRangeDetails()
+        {
+            Console.WriteLine("Enter Lower limit of factor");
+            lowerLimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Upper limit of factor");
+            upperLimit = Convert.ToInt32(Console.ReadLine());
+        }
+        public void CalculatePossibleFactors()
+        {
+            Console.WriteLine("The possible factors for the provided Range are");
+            for (int factor = lowerLimit; factor < upperLimit; factor++)
+            {
+                Console.WriteLine(factor * factor - 2 * factor + 1);
+                Console.ReadLine();
+            }
+        }
+    }
+    
+}
