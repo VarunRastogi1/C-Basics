3a38b9642df279e8f361ad77de33077720693c00
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-06-24 22:55:52 +0530

    adding MaxMinRating.cs and updating main

---
+++ EmployeeDetails/EmployeeDetails/MaxMinRating.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDetails
+{
+    public class MaxMinRating
+    {
+        int numEmp;
+        int[] arrRating = new int[100];
+        public void GetDetails()
+        {
+
+            Console.WriteLine("Enter number of employees");
+            numEmp = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < numEmp; i++)
+            {
+                Console.WriteLine("Enter rating of employee {0}", i + 1);
+                arrRating[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public void ShowMaxRating()
+        {
+            int maxRating = arrRating[0];
+            for (int i = 0; i < numEmp; i++)
+            {
+                if (maxRating < arrRating[i])
+                {
+                    maxRating = arrRating[i];
+                }
+            }
+            Console.WriteLine("Maximum Rating={0}", maxRating);
+        }
+        public void ShowMinRating()
+        {
+            int minRating = arrRating[0];
+            for (int i = 0; i < numEmp; i++)
+            {
+                if (minRating > arrRating[i])
+                {
+                    minRating = arrRating[i];
+                }
+            }
+            Console.WriteLine(" Minimum Rating={0}", minRating);
+        }
+       
+    }
+}
