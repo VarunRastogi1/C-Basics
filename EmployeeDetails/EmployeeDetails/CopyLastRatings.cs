dd6be68b6d9d5c9842dc8d306dccb2123c7c5d7a
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-06-24 23:55:42 +0530

    adding CopyLastRatings.cs and updating main

---
+++ EmployeeDetails/EmployeeDetails/CopyLastRatings.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDetails
+{
+    public class CopyLastRating
+    {
+        int[] ratingLastYear = new int[5];
+        int[] ratingThisYear = new int[5];
+        public void GetLastYearRating()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter last year rating of employee {0}", i + 1);
+                ratingLastYear[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public void CopyRating()
+        {         
+            for (int i = 0; i<ratingThisYear.Length; i++)
+            {
+                ratingThisYear[i] = ratingLastYear[i];
+            }
+        }
+        public void ShowThisYearRating()
+        {
+            Console.WriteLine("--------------After copy the ratings of last year-------------");
+            for (int i = 0; i < ratingThisYear.Length; i++)
+            {
+                Console.WriteLine(ratingThisYear[i]);
+                Console.ReadLine();
+            }
+        }
+    }
+    
+}
