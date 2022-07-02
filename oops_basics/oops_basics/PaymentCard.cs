703200dc57f5b6fe9a4e5db4cb17d564eceee3c9
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-07-02 14:25:03 +0530

    Adding PaymentCard

---
+++ oops_basics/oops_basics/PaymentCard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_basics
+{
+    public class PaymentCard
+    {
+        public int OpeningBalance { get; set; }
+        public PaymentCard(int openingBalance)
+        {
+            OpeningBalance = openingBalance;    
+        }
+
+        public override string ToString()
+        {
+            return $"The card has a balance of {OpeningBalance} Euros";
+        }
+    }
+}
