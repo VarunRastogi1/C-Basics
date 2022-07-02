6088d1206ee752cbef3b9059f160589b8e1995a7
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-07-02 16:30:13 +0530

    Adding Book class

---
+++ oops_basics/oops_basics/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_basics
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int PageCount { get; set; }
+
+        public int PublicationYear { get; set; }
+
+        public Book(string title,int pageCount, int publicationYear)
+        {
+            Title= title;
+            PageCount= pageCount;
+            PublicationYear= publicationYear;
+        }
+
+    }
+}
