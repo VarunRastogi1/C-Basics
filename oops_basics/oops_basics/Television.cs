65c92ca7e342385acec09d684d30975f41c3b392
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-07-03 20:01:14 +0530

    Ceaning Television class

--- oops_basics/oops_basics/Television.cs
+++ oops_basics/oops_basics/Television.cs
@@ -5,24 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace oops_basics
+{
+    public class Television
     {
-        public class Television
+        public string Name { get; set; }
+        public int Duration { get; set; }
+
+        public Television(string name, int duration)
         {
-            public string Name { get; set; }
-            public int Duration { get; set; }
-
-            public Television(string name, int duration)
-            {
-                Name = name;
-                Duration = duration;
-            }
-        }
-    }
-
-
-    Console.ReadLine();
+            Name = name;
+            Duration = duration;
         }
     }
 }
-3
+
+
+   
