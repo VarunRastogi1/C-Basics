33cc4596c9ef8c246453fda76826c3f82a7d5256
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-07-03 13:32:19 +0530

    Updating Program.cs

--- oops_basics/oops_basics/Program.cs
+++ oops_basics/oops_basics/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Collections.Generic;
+
 namespace oops_basics
 {
     public class Program
@@ -10,21 +12,42 @@ namespace oops_basics
             int myPageCount;
             int myPublicationYear;
 
-            for(int i=0; myTitle == "";i++)
+            List<Book> books = new List<Book>();
+            while(true)
             {
                 myTitle = Console.ReadLine();
+                if(myTitle == "")
+                {
+                    break;
+                }
                 myPageCount= int.Parse(Console.ReadLine());
                 myPublicationYear= int.Parse(Console.ReadLine());
                 Book myBook = new Book(myTitle, myPageCount, myPublicationYear);
+                books.Add(myBook);
             }
-
-
+            Console.WriteLine("what are you looking for");
+            string search= Console.ReadLine();
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (search == "everything")
+                {
+                    foreach (Book obj in books)
+                    {
+                        Console.WriteLine(obj.ToString());
+                    }
+                    break;
+                }
+                else if (search == books[i].Title)
+                {
+                    Console.WriteLine($"Title: {books[i].Title} PageCount: {books[i].PageCount} PublicationYear: { books[i].PublicationYear}");
+                }
+            }
             var card = new PaymentCard(15);
             card.EatLunch();
             card.DrinkCoffee();
             Console.WriteLine(card.ToString());
-
             card.ChargeCard(10);
+            Console.ReadLine();
         }       
     }
 }
