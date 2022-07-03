00469e8448f58e9ad2ad326b53f011a10468fd96
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-07-03 18:42:05 +0530

    Adding Television class

---
+++ oops_basics/oops_basics/Television.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_basics
+    {
+        public class Television
+        {
+            public string Name { get; set; }
+            public int Duration { get; set; }
+
+            public Television(string name, int duration)
+            {
+                Name = name;
+                Duration = duration;
+            }
+        }
+    }
+
+
+    Console.ReadLine();
+        }
+    }
+}
+3
