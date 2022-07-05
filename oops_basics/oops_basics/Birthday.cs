35e43993191e2f8f50f9500b3885178b0dfe59d7
Varun Rastogi <contactvarunrastogi@gmail.com>
2022-07-06 00:24:01 +0530

    Adding Birthday class

---
+++ oops_basics/oops_basics/Birthday.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_basics
+{
+    public class Birthday
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+
+        public Birthday(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+        public int CalculateAge()
+        {
+            int age = DateTime.Now.Year - Year;
+            return age;
+        }
+        public void Validate()
+        {
+            if (CalculateAge() > 135)
+            {
+                Console.WriteLine("Not Possible");
+            }
+            else if (CalculateAge() < 0)
+            {
+                throw new Exception("Invalid Value");
+            }
+            else
+            {
+                Console.WriteLine($"Your Age is {CalculateAge()}");
+            }
+        }
+    }
+}
